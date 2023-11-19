f473e08757f1ba820d227ed138ec0822cf2ec991
Rocky9463 <rohit94639463@gmail.com>
2023-11-19 12:29:26 +0530

    options for resolution

--- yt_dl.py
+++ yt_dl.py
@@ -7,9 +7,25 @@ except ImportError:
 
 link = "https://youtu.be/vEQ8CXFWLZU?si=tvFETuz7YLn44flR"
 print(link)
-yt = pytube.YouTube(link)
-print("Title : ", yt.title)
+yt_video = pytube.YouTube(link)
+#print("Title : ", yt.title)
+print(f"""
+Title: {yt_video.title}
+Length: {yt_video.length/60} minutes
+Date Published: {yt_video.publish_date}
+Views: {yt_video.views}
+""")
 
-#check
+streams = set()
+
+for stream in yt_video.streams.filter(type="video"):
+    streams.add(stream.resolution)
+print("Availabel formats : ", streams)
+
+for stream in yt_video.streams.filter(resolution="1440p"):
+    print(stream)
+    #stream.download()
+
+
 
 # WORK IN PROGRESS
